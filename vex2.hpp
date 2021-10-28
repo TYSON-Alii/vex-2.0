e74e1429ff471c8c1f8c5fd93b71dd63fad8dbf4
tyson <65432197+TYSON-Alii@users.noreply.github.com>
2021-10-28 15:17:13 +0300

    Update vex2.hpp

--- vex2.hpp
+++ vex2.hpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 template <typename T>
 struct vex2 {
     T x, y;
@@ -6,24 +8,73 @@ struct vex2 {
 
     template <typename _T>
     vex2(_T v) { x = y = T(v); };
+    template <> vex2(std::string v);
     
     template <typename _T1, typename _T2>
     vex2(_T1 v1, _T2 v2) { x = T(v1); y = T(v2); };
+    template <> vex2(std::string v1, std::string v2);
 
     template <typename _T>
     vex2<T>& operator=(_T v) { x = y = T(v); return *this; };
+    template <> vex2<T>& operator=(std::string v);
+    template<typename _T>
+    vex2<T>& operator=(vex2<_T> v) { x = T(v.x); y = T(v.y); return *this; };
+
+    template <typename _T>
+    vex2<T>& operator+=(_T v) { x += T(v); y += T(v); return *this; };
+    template<typename _T>
+    vex2<T>& operator+=(vex2<_T> v) { x += T(v.x); y += T(v.y); return *this; };
+
+    template <typename _T>
+    vex2<T>& operator-=(_T v) { x -= T(v); y -= T(v); return *this; };
+    template<typename _T>
+    vex2<T>& operator-=(vex2<_T> v) { x -= T(v.x); y -= T(v.y); return *this; };
+
+    template <typename _T>
+    vex2<T>& operator*=(_T v) { x *= T(v); y *= T(v); return *this; };
+    template<typename _T>
+    vex2<T>& operator*=(vex2<_T> v) { x *= T(v.x); y *= T(v.y); return *this; };
+
+    template <typename _T>
+    vex2<T>& operator/=(_T v) { x /= T(v); y /= T(v); return *this; };
+    template<typename _T>
+    vex2<T>& operator/=(vex2<_T> v) { x /= T(v.x); y /= T(v.y); return *this; };
+
+    template <typename _T>
+    vex2<T> operator+(_T v) { return vex2<T>(this->x + v, this->y + v); };
+    template<typename _T>
+    vex2<T> operator+(vex2<_T> v) { return vex2<T>(this->x + v.x, this->y + v.y); };
+
+    template <typename _T>
+    vex2<T> operator-(_T v) { return vex2<T>(this->x - v, this->y - v); };
+    template<typename _T>
+    vex2<T> operator-(vex2<_T> v) { return vex2<T>(this->x - v.x, this->y - v.y); };
+
+    template <typename _T>
+    vex2<T> operator*(_T v) { return vex2<T>(this->x * v, this->y * v); };
+    template<typename _T>
+    vex2<T> operator*(vex2<_T> v) { return vex2<T>(this->x * v.x, this->y * v.y); };
+
+    template <typename _T>
+    vex2<T> operator/(_T v) { return vex2<T>(this->x / v, this->y / v); };
+    template<typename _T>
+    vex2<T> operator/(vex2<_T> v) { return vex2<T>(this->x / v.x, this->y / v.y); };
+
+    friend std::ostream& operator<<(std::ostream& os, const vex2<T>& v) { os << v.x << ' ' << v.y; return os; };
 };
 
-template<> template<> vex2<float>::vex2<std::string>(std::string v) { x = y = std::atof(v.c_str()); };
-template<> template<> vex2<float>::vex2<std::string, std::string>(std::string v1, std::string v2) { x = std::atof(v1.c_str()); y = std::atof(v2.c_str()); };
-template<> template<> vex2<float>& vex2<float>::operator=<vex2<int>>(vex2<int> v) { x = v.x; y = v.y; return *this; };
+template<> template<> vex2<float>::vex2(std::string v)  { x = y = std::atof(v.c_str()); };
+template<> template<> vex2<int>::vex2(std::string v)    { x = y = std::atoi(v.c_str()); };
+template<> template<> vex2<double>::vex2(std::string v) { x = y = std::atof(v.c_str()); };
 
-template<> template<> vex2<int>::vex2<std::string>(std::string v) { x = y = std::atof(v.c_str()); };
-template<> template<> vex2<int>::vex2<std::string, std::string>(std::string v1, std::string v2) { x = std::atoi(v1.c_str()); y = std::atoi(v2.c_str()); };
+template<> template<> vex2<float>::vex2(std::string v1, std::string v2) { x = std::atof(v1.c_str()); y = std::atof(v2.c_str()); };
+template<> template<> vex2<int>::vex2(std::string v1, std::string v2)   { x = std::atof(v1.c_str()); y = std::atoi(v2.c_str()); };
+template<> template<> vex2<double>::vex2(std::string v1, std::string v2){ x = std::atof(v1.c_str()); y = std::atof(v2.c_str()); };
 
-template<> template<> vex2<double>::vex2<std::string>(std::string v) { x = y = std::atof(v.c_str()); };
-template<> template<> vex2<double>::vex2<std::string, std::string>(std::string v1, std::string v2) { x = std::atof(v1.c_str()); y = std::atof(v2.c_str()); };
-
+template<> template<> vex2<float>& vex2<float>::operator=(std::string v)    { x = y = std::atof(v.c_str()); return *this; };
+template<> template<> vex2<int>& vex2<int>::operator=(std::string v)        { x = y = std::atoi(v.c_str()); return *this; };
+template<> template<> vex2<double>& vex2<double>::operator=(std::string v)  { x = y = std::atof(v.c_str()); return *this; };
 
 typedef vex2<float> vex2f;
-typedef vex2<int> vex2f;
+typedef vex2<int> vex2i;
+typedef vex2<double> vex2d;
